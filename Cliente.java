739293bd9152ac2d013bfe79a9a7a41e45958efc
yeilis10 <yvorozcog@academia.usbbog.edu.co>
2026-03-24 09:27:19 -0500

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,19 +1,19 @@
 public class Cliente {
-    // 1. Atributos privados
-    private int id;
+    private String idCliente;
     private String nombre;
     private String telefono;
 
-    // 2. Constructor
-    public Cliente(int id, String nombre, String telefono) {
-        this.id = id;
+    public Cliente(String idCliente, String nombre, String telefono) {
+        this.idCliente = idCliente;
         this.nombre = nombre;
         this.telefono = telefono;
     }
 
-    // 3. Getters y Setters
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    
-    // ... otros métodos ...
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void realizarCompra(Venta v) {
+        System.out.println("Cliente " + nombre + " realizó una compra.");
+    }
 }
